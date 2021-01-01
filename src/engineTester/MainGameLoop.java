e511d48a19747a8d0bddf8ae86235e83ddcc0f50
Smbo-2002 <smbo.voskanyan2002@gmail.com>
2021-01-02 00:24:07 +0400

    class 5: Coloring using Shaders

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -5,39 +5,44 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
-    public static void main(String[] args) {
-        DisplayManager.createDisplay();
 
+    public static void main(String[] args) {
+
+        DisplayManager.createDisplay();
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
 
         float[] vertices = {
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f
+                -0.5f,0.5f,0,	//V0
+                -0.5f,-0.5f,0,	//V1
+                0.5f,-0.5f,0,	//V2
+                0.5f,0.5f,0		//V3
         };
 
         int[] indices = {
-                0, 1, 3,
-                3, 1, 2
+                0,1,3,	//Top left triangle (V0,V1,V3)
+                3,1,2	//Bottom right triangle (V3,V1,V2)
         };
 
-        RawModel model = loader.loadVAO(vertices, indices);
+        RawModel model = loader.loadToVAO(vertices,indices);
 
-        while(!Display.isCloseRequested()) {
+        while(!Display.isCloseRequested()){
+            //game logic
             renderer.prepare();
-            // game logic
-            // render
+            shader.start();
             renderer.render(model);
+            shader.stop();
             DisplayManager.updateDisplay();
         }
 
+        shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
+
     }
+
 }
