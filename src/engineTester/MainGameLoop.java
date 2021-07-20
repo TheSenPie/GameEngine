2e36c9b147bc81f27dd3d962dd430560e2642a8e
senpie <smbo.voskanyan2002@gmail.com>
2021-07-21 00:27:03 +0400

    class 11: light source added to the game

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,6 +2,7 @@ package engineTester;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
@@ -101,14 +102,15 @@ public class MainGameLoop {
 //        };
 
 //        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-        RawModel model = OBJLoader.loadObjMode("stall", loader);
+        RawModel model = OBJLoader.loadObjMode("dragon", loader);
 
 //        ModelTexture texture = new ModelTexture(loader.loadTexture("eye"));
-        ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+        ModelTexture texture = new ModelTexture(loader.loadTexture("textured-stripes"));
 
         TexturedModel staticModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
+        Entity entity = new Entity(staticModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);
+        Light light = new Light(new Vector3f(0, -5, -20), new Vector3f(1, 1, 1));
 
         Camera camera  = new Camera();
 
@@ -117,6 +119,7 @@ public class MainGameLoop {
             camera.move();
             renderer.prepare();
             shader.start();
+            shader.loadLight(light);
             shader.loadViewMatrix(camera);
             renderer.render(entity, shader);
             shader.stop();
