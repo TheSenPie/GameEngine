6355e1cf1d90727ec51e23842d6e063b1aca2858
senpie <smbo.voskanyan2002@gmail.com>
2021-07-23 00:42:04 +0400

    class 13: ambient lightning added, rendering optimization

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,13 +6,13 @@ import entities.Light;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
+import renderEngine.*;
 import models.RawModel;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainGameLoop {
 
@@ -20,115 +20,44 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
 
-//        float[] vertices = {
-//                -0.5f,0.5f,-0.5f,
-//                -0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,-0.5f,
-//                0.5f,0.5f,-0.5f,
-//
-//                -0.5f,0.5f,0.5f,
-//                -0.5f,-0.5f,0.5f,
-//                0.5f,-0.5f,0.5f,
-//                0.5f,0.5f,0.5f,
-//
-//                0.5f,0.5f,-0.5f,
-//                0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,0.5f,
-//                0.5f,0.5f,0.5f,
-//
-//                -0.5f,0.5f,-0.5f,
-//                -0.5f,-0.5f,-0.5f,
-//                -0.5f,-0.5f,0.5f,
-//                -0.5f,0.5f,0.5f,
-//
-//                -0.5f,0.5f,0.5f,
-//                -0.5f,0.5f,-0.5f,
-//                0.5f,0.5f,-0.5f,
-//                0.5f,0.5f,0.5f,
-//
-//                -0.5f,-0.5f,0.5f,
-//                -0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,-0.5f,
-//                0.5f,-0.5f,0.5f
-//
-//        };
-//
-//        int[] indices = {
-//                0,1,3,
-//                3,1,2,
-//                4,5,7,
-//                7,5,6,
-//                8,9,11,
-//                11,9,10,
-//                12,13,15,
-//                15,13,14,
-//                16,17,19,
-//                19,17,18,
-//                20,21,23,
-//                23,21,22
-//
-//        };
-//
-//        float[] textureCoords = {
-//
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0,
-//                0,0,
-//                0,1,
-//                1,1,
-//                1,0
-//
-//        };
-
-//        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
         RawModel model = OBJLoader.loadObjMode("dragon", loader);
-
-//        ModelTexture texture = new ModelTexture(loader.loadTexture("eye"));
 
         TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
 
-        Entity entity = new Entity(staticModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);
         Light light = new Light(new Vector3f(0, -5, -20), new Vector3f(0.9529411764705882f, 0.7803921568627451f, 0.8549019607843137f));
+
+        Camera camera  = new Camera();
+
         ModelTexture texture = staticModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(1);
 
-        Camera camera  = new Camera();
+        List<Entity> entities = new ArrayList<>();
+        for(int i = 0; i <= 100; i++) {
+            float x = (float) Math.random() * 100 - 50;
+            float y = (float) Math.random() * 100 - 50;
+            float z = (float) Math.random() * 100 - 50;
+
+            Entity entity = new Entity(staticModel, new Vector3f(x, y, z), (float) Math.random() * 180f, (float) Math.random() * 180f, 0, 1);
+            entities.add(entity);
+        }
+
+        MasterRenderer renderer = new MasterRenderer();
 
         while(!Display.isCloseRequested()){
-            entity.increaseRotation(0, 1, 0);
             camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity, shader);
-            shader.stop();
+
+            for(Entity entity : entities) {
+                entity.increaseRotation(0, 1, 0);
+                renderer.processEntity(entity);
+            }
+
+            renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
 
-        shader.cleanUp();
+        renderer.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
 
